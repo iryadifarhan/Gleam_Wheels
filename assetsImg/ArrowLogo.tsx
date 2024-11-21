37dd6d19d1a8b0a2a6d9d6b5c0e1e73116e78524
iryadifarhan <128307091+iryadifarhan@users.noreply.github.com>
2024-11-21 10:06:08 +0700

    Add files via upload

---
+++ assetsImg/ArrowLogo.tsx
@@ -0,0 +1,8 @@
+export const ArrowLogo = () => {
+    return(
+        <svg width="13" height="20" viewBox="0 0 13 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M11 2L2 10L11 18" stroke="white" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+
+    )
+}
